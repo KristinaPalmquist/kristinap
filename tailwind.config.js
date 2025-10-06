4026bdc4a0cff14302d977788c85cb05335a6feb
Kristina <kristina.palmquist@gmail.com>
2025-10-06 15:21:04 +0200

    moving repo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./test.html",
+    "./templates/pages/home.html",
+    "./templates/**/*.html",
+    "./**/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        hotpink: "#FF69B4",
+        lightpink: "#FFD1DC",
+        brightyellow: "#FFEB3B",
+      },
+    },
+  },
+  plugins: [],
+};
